6e1b1d20b73d8b0a92c545f1553494fd15e0c68c
Ist4lri <pierreloic.gouttebel@gmail.com>
2022-11-23 14:28:45 +0100

    Find a way to open Terminal on Linux

--- shellTest.py
+++ shellTest.py
@@ -1,7 +1,7 @@
 import subprocess
 
 if (__file__.startswith('/home')):
-    print("Linux !")
+    subprocess.Popen(("gnome-terminal.real", "/usr/bin"))
 elif (__file__.startswith('c:\\')):
     print("Windows !")
 else:
