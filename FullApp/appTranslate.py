4e2cf030b44d2d4d3d6d2a0928f34574114e807c
Ist4lri <pierreloic.gouttebel@gmail.com>
2022-12-04 22:19:38 +0100

    Add menu, and fixing Canvas Position

--- FullApp/appTranslate.py
+++ FullApp/appTranslate.py
@@ -52,7 +52,17 @@ def trans_trad(trad, type):
 
 def choose():
     # Si le chemin du fichier est présent, on va envoyer la demande de faire la trans/trad à partir du fichier.
-    if filename:
+    if seq.get("1.0", "end") != "\n":
+        Dif = Toplevel()
+        Button(Dif, text="ADN -> ARN", command=lambda: [
+            Dif.destroy(), trans_trad("ADAR", "seq")]).grid(row=0, column=0)
+        Button(Dif, text="ARN -> PROTEINES", command=lambda: [
+            Dif.destroy(), trans_trad("ARP", "seq")]).grid(row=0, column=1)
+        Button(Dif, text="ADN -> PROTEINES", command=lambda: [
+            Dif.destroy(), trans_trad("ADP", "seq")]).grid(row=0, column=2)
+        Button(Dif, text="Quitter",
+               command=Dif.destroy).grid(row=1, column=1)
+    else:
         Dif = Toplevel()
         Button(Dif, text="ADN -> ARN", command=lambda: [
             Dif.destroy(), trans_trad("ADAR", "file")]).grid(row=0, column=0)
@@ -63,16 +73,6 @@ def choose():
         Button(Dif, text="Quitter",
                command=Dif.destroy).grid(row=1, column=1)
     # Si la zone de texte est présente, on va envoyer la demande de faire la trans/trad a partir du contenus de la zone de texte.
-    elif seq.get("1.0", "end"):
-        Dif = Toplevel()
-        Button(Dif, text="ADN -> ARN", command=lambda: [
-            Dif.destroy(), trans_trad("ADAR", "seq")]).grid(row=0, column=0)
-        Button(Dif, text="ARN -> PROTEINES", command=lambda: [
-            Dif.destroy(), trans_trad("ARP", "seq")]).grid(row=0, column=1)
-        Button(Dif, text="ADN -> PROTEINES", command=lambda: [
-            Dif.destroy(), trans_trad("ADP", "seq")]).grid(row=0, column=2)
-        Button(Dif, text="Quitter",
-               command=Dif.destroy).grid(row=1, column=1)
 
 
 ################################################################################
@@ -94,11 +94,35 @@ def keyPress(event):
 MainApp = Tk()
 MainApp.title("Transcription/Translation App")
 global w, h
+global filename
 w = MainApp.winfo_screenwidth()
 h = MainApp.winfo_screenheight()
 MainApp.geometry(f'{w}x{h}')
-MainApp.bind("<KeyPress>", keyPress)
 
+################################################################################
+######################## GENERATION DE LA BARRE DE MENU ########################
+################################################################################
+
+
+def pong():
+    print("pong")
+
+
+barMenu = Menu(MainApp)
+fileMenu = Menu(barMenu, tearoff=0)
+fileMenu.add_command(label="Enregistrer", command=pong)
+fileMenu.add_command(label="Enregistrer Sous", command=pong)
+fileMenu.add_separator()
+fileMenu.add_command(label="Quitter", command=MainApp.destroy)
+barMenu.add_cascade(label="Fichier", menu=fileMenu)
+
+editMenu = Menu(barMenu, tearoff=0)
+editMenu.add_command(label="Copier la séquence résultat", command=pong)
+barMenu.add_cascade(label="Edition", menu=editMenu)
+
+actionMenu = Menu(barMenu, tearoff=0)
+actionMenu.add_command(label="Requête BLAST NCBI", command=pong)
+barMenu.add_cascade(label="Action", menu=actionMenu)
 
 ################################################################################
 ###################### GENERATION DE LA ZONE DE RESULTATS ######################
@@ -121,6 +145,7 @@ lseq = Label(inLabel, text="Entrez votre séquence ici :")
 lseq.pack(padx=5, pady=10)
 seq = Text(inLabel, width=100, cursor="trek", state=NORMAL)
 seq.pack(padx=5, pady=10)
+MainApp.bind("<KeyPress>", keyPress)
 Button(inLabel, text='Choisissez en quoi voulez vous le convertir',
        command=choose).pack(padx=10, pady=10)
 fButton = Button(inLabel, text='Charger un fichier FASTA',
@@ -135,5 +160,5 @@ fButton.pack(padx=10, pady=10)
 quit = Button(inLabel, text='Quitter', command=MainApp.destroy)
 quit.pack(padx=10, pady=10)
 
-
+MainApp.config(menu=barMenu)
 MainApp.mainloop()
